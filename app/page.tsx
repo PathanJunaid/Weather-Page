e500a7b107da38e8981b4b1662d5e77b047f2469
Junaid <me.zunaidkhan@gmail.com>
2024-08-28 19:41:51 +0530

    CSS added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "./Context/StoreContext";
 import HeroSection from "@/Components/HeroSection";
 import Weather from "@/Components/Weather";
-import Loading from '@/Components/Loading'
+import Loading from '@/Components/Loading';
+import './globals.css';
 export default function Home() {
   const {
     getcurrentlocation,
@@ -17,7 +18,6 @@ export default function Home() {
       const success = await getcurrentlocation();
       if (success) {
         setload(false);
-
         console.log("Location fetched successfully");
       } else {
         setload(true);
@@ -28,6 +28,7 @@ export default function Home() {
       handleLocationFetch();
     } else {
       getweatherdata();
+      setload(false)
     }
   }, [load, position]);
   if (load || Pageload) {
